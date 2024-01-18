353bb80da9afc692845943f3fc7aff5976fc574d
liamo3323 <liam3323@gmail.com>
2024-01-18 15:09:44 +0000

    merged all files to paxos

--- paxos_solo.ex
+++ paxos_solo.ex
@@ -1,3 +1,23 @@
+# # TASK 2 HANDLING
+# def test(paxos_pid) do
+#   propose = Paxos.propose(paxos_pid, :inst_1, :value_1, 1000)
+
+#   handle(propose)
+#  end
+
+#  def handle({:abort}) do
+#    1
+#  end
+
+#  def handle({:timeout}) do
+#    2
+#  end
+
+#  def handle({:decision, v}) do
+#   3
+#  end
+
+# TASK 1
 defmodule Paxos do
   def start(name, participants) do
     # do spawn stuff
@@ -365,255 +385,6 @@ defmodule Paxos do
         {:abort}
     after
       t -> {:timeout}
-    end
-  end
-end
-
-defmodule Leader_election do
-  # pick lowest process
-  # processes wait to hear back
-  # timeout  if nothing heard
-  # when heard back pick leader
-  # if wrong leader elected, Paxos will reject proposals, because not leader (if check)
-
-  def start(name, participants) do
-    pid = spawn(Leader_election, :init, [name, participants])
-
-    Utils.register_name(Utils.add_to_name(name, "_LeaderElec"), pid)
-
-    # case :global.re_register_name(Utils.add_to_name(name, "_LeaderElec"), pid) do
-    #     :yes -> pid
-    #     :no  -> :error
-    # end
-    # Process.link(pid)
-    # IO.puts "registered #{name}"
-    # pid
-  end
-
-  def init(name, participants) do
-    state = %{
-      name: Utils.add_to_name(name, "_LeaderElec"),
-      # update to be paxos process name
-      parent_name: name,
-      participants: Enum.map(participants, fn n -> Utils.add_to_name(n, "_LeaderElec") end),
-      alive: %MapSet{},
-      leader: nil,
-      timeout: 100
-    }
-
-    Process.send_after(self(), {:timeout}, 1000)
-
-    run(state)
-  end
-
-  def run(state) do
-    state =
-      receive do
-        {:timeout} ->
-          # send heartbeat out to list of processes
-          # Await using delay of timeout
-          # sort the alive
-          # Assign leader from list of replied processes
-          # Clear the list
-
-          # IO.puts("#{state.name}: #{inspect({:timeout})}")
-
-          Utils.beb_broadcast(state.participants, {:heartbeat_req, self()})
-
-          Process.send_after(self(), {:timeout}, state.timeout)
-
-          state = elec_leader(state)
-
-          %{state | alive: %MapSet{}}
-
-        {:heartbeat_req, pid} ->
-          # IO.puts("#{state.name}: #{inspect({:heartbeat_req, pid})}")
-          send(pid, {:heartbeat_reply, state.parent_name})
-          state
-
-        {:heartbeat_reply, name} ->
-          # IO.puts("#{state.name}: #{inspect {:heartbeat_reply, name}}")
-          %{state | alive: MapSet.put(state.alive, name)}
-      end
-
-    run(state)
-  end
-
-  defp elec_leader(state) do
-    # when more than 1 process is alive
-    if MapSet.size(state.alive) > 0 do
-      # sort the list and make the first element the leader
-      first_elem = Enum.at(Enum.sort(state.alive), 0)
-      # if first elem is already leader dont elect new leader
-      if first_elem != state.leader do
-        Utils.unicast(state.parent_name, {:leader_elect, first_elem})
-        # the new leader is set
-        %{state | leader: first_elem}
-      else
-        state
-      end
-    else
-      state
-    end
-  end
-end
-
-defmodule EagerReliableBroadcast do
-  def start(name, processes, upper) do
-      pid = spawn(EagerReliableBroadcast, :init, [name, processes, upper])
-      # :global.unregister_name(name)
-      case :global.re_register_name(name, pid) do
-          :yes -> pid
-          :no  -> :error
-      end
-      IO.puts "registered #{name}"
-      pid
-  end
-
-  # Init event must be the first
-  # one after the component is created
-  def init(name, processes, upper_layer) do
-      state = %{
-          name: name,
-          processes: processes,
-          upper_layer: upper_layer,
-
-          # Tracks the sets of message sequence numbers received from each process
-          delivered: (for p <- processes, into: %{}, do: {p, %MapSet{}}),
-
-          # Tracks the highest contiguous message sequence number received from each process
-          all_up_to: (for p <- processes, into: %{}, do: {p, -1}),
-
-          # Current sequence number to assign to a newly broadcast message
-          seq_no: 0
-      }
-      run(state)
-  end
-
-  def run(state) do
-      state = receive do
-          {:broadcast, m} ->
-              data_msg = {:data, state.name, state.seq_no, m}
-              state = %{state | seq_no: state.seq_no + 1}
-              beb_broadcast(data_msg, state.processes)
-
-              # Replace beb_broadcast above with the one below to
-              # simulate the scenario in which :p0 does not broadcast to :p0 and :p1, and
-              # no one broadcasts to :p0.
-              # beb_broadcast_with_failures(state.name, :p0, [:p0, :p1], data_msg, state.processes)
-
-              # IO.puts("#{inspect state.name}: RB-broadcast: #{inspect m}")
-              state
-
-          {:data, proc, seq_no, m} ->
-              # IO.puts("#{inspect state.name}: BEB-deliver: #{inspect m} from #{inspect proc}, seqno=#{inspect seq_no}")
-              delivered = state.delivered[proc]
-              all_up_to = state.all_up_to[proc]
-              # IO.puts("#{inspect state.name}: delivered=#{inspect delivered}, all_up_to=#{inspect all_up_to}, seq_no=#{inspect seq_no}")
-
-              # This is a new message
-              if seq_no > all_up_to and seq_no not in delivered do
-                  # IO.puts("#{inspect state.name}: New message: #{inspect m} from #{inspect proc}, seqno=#{inspect seq_no}")
-                  delivered = MapSet.put(delivered, seq_no) # update delivered of proc with a new sequence number
-                  {delivered, all_up_to} = compact_delivered(delivered, all_up_to)  # Try to compact delivered and update all_up_to
-                  send(state.upper_layer, {:rb_deliver, proc, m}) # Trigger deliver indiciation
-                  beb_broadcast({:data, proc, seq_no, m}, state.processes) # Rebroadcast the message to ensure Agreement
-
-                  # Replace beb_broadcast above with the one below to
-                  # simulate the scenario in which :p0 does not broadcast to :p0 and :p1, and
-                  # no one broadcasts to :p0.
-                  # beb_broadcast_with_failures(state.name, :p0, [:p0, :p1], data_msg, state.processes)
-                  # beb_broadcast_with_failures(state.name, :p0, [:p0, :p1, :p2], {:data, proc, seq_no, m}, state.processes)
-
-                  # IO.puts("#{inspect state.name}: Echo: #{inspect m} from #{inspect proc}, seqno=#{inspect seq_no}")
-
-                  # Update the state to reflect updates to delivered and all_up_to
-                  %{state | delivered: %{state.delivered | proc => delivered},
-                            all_up_to: %{state.all_up_to | proc => all_up_to}}
-              else
-                  # IO.puts("#{inspect state.name}: Delivered before: #{inspect m} from #{inspect proc}, seqno=#{inspect seq_no}")
-                  state
-              end
-      end
-      run(state)
-  end
-
-  # Compute the next highest contiguous message sequence number in the set s
-  # starting from seqno
-  defp get_upper_seqno(seqno, s) do
-      if seqno in s, do: get_upper_seqno(seqno + 1, s), else: seqno-1
-  end
-
-  # Try to compact the delivered set for a process
-  # as per the algorithm described in the handout
-  defp compact_delivered(delivered, all_up_to) do
-      new_upper_seqno = get_upper_seqno(all_up_to + 1, delivered)
-      if new_upper_seqno > all_up_to do
-          delivered = Enum.reduce(all_up_to + 1..new_upper_seqno, delivered,
-                  fn(sn, s) -> MapSet.delete(s, sn) end)
-          all_up_to = new_upper_seqno
-          {delivered, all_up_to}
-      else
-          {delivered, all_up_to}
-      end
-  end
-
-  # Send message m point-to-point to process p
-  defp unicast(m, p) do
-      case :global.whereis_name(p) do
-              pid when is_pid(pid) -> send(pid, m)
-              :undefined -> :ok
-      end
-  end
-
-  # Best-effort broadcast of m to the set of destinations dest
-  defp beb_broadcast(m, dest), do: for p <- dest, do: unicast(m, p)
-
-  # Simulate a scenario in which proc_to_fail fails to send the message m
-  # to the processes in fail_send_to, and all other processes fail
-  # to send to proc_to_fail
-  defp beb_broadcast_with_failures(name, proc_to_fail, fail_send_to, m, dest) do
-      if name == proc_to_fail do
-          for p <- dest, p not in fail_send_to, do: unicast(m, p)
-      else
-          for p <- dest, p != proc_to_fail, do: unicast(m, p)
-      end
-  end
-
-end
-
-defmodule Utils do
-  def unicast(p, m) when p == nil, do: IO.puts("!!! Sending to nil with val #{inspect(m)}")
-
-  def unicast(p, m) do
-    case :global.whereis_name(p) do
-      pid when is_pid(pid) -> send(pid, m)
-      :undefined -> :ok
-    end
-  end
-
-  # Best-effort broadcast of m to the set of destinations dest
-  def beb_broadcast(dest, m), do: for(p <- dest, do: unicast(p, m))
-
-  def add_to_name(name, to_add), do: String.to_atom(Atom.to_string(name) <> to_add)
-
-  # \\ means default
-  def register_name(name, pid, link \\ true) do
-    case :global.re_register_name(name, pid) do
-      :yes ->
-        # parent runs this to link to leader + ERB
-        # when one dies all links also die
-
-        # if link true
-        if link do
-          Process.link(pid)
-        end
-
-        pid
-
-      :no ->
-        Process.exit(pid, :kill)
-        :error
     end
   end
 end
